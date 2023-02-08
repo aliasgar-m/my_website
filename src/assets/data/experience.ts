e3e2025e0c4f7b73f1ec053ab7818bccb121f70e
Aliasgar Merchant <amerch23@uic.edu>
2023-02-08 13:38:16 -0600

    fixed image problem

--- src/assets/data/experience.ts
+++ src/assets/data/experience.ts
@@ -1,21 +1,21 @@
 export const experiences = [
     {
         name: "University of Illinois at Chicago",
-        logo: "../assets/images/uic_logo.png",
+        logo: "assets/images/uic_logo.png",
         job_title: "Student Ambassador - UIC Career Fair (Volunteer)",
         duration: "September 2022"
     },
     
     {
         name: "Orion Racing India",
-        logo: "../assets/images/orion_logo.png",
+        logo: "assets/images/orion_logo.png",
         job_title: "Software Engineer - Driverless Vehicle Project",
         duration: "August 2020 - February 2022"
     },
 
     {
         name: "Riidl",
-        logo: "../assets/images/riidl_logo.png",
+        logo: "assets/images/riidl_logo.png",
         job_title: "CAD Designer",
         duration: "June 2019 - October 2019"
     }
